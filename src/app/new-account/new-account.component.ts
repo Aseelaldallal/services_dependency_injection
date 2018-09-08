87cee571f2192c246d37d2744e4004b67c1fd0d4
Aseel Al Dallal <aseelaldallal@gmail.com>
2018-09-08 18:54:12 -0400

    inject logging service, provide it

--- src/app/new-account/new-account.component.ts
+++ src/app/new-account/new-account.component.ts
@@ -4,13 +4,15 @@ import { LoggingService } from '../logging.service';
 @Component({
   selector: 'app-new-account',
   templateUrl: './new-account.component.html',
-  styleUrls: ['./new-account.component.css']
+  styleUrls: ['./new-account.component.css'],
+  providers: [LoggingService] // Providing, so it can inject
 })
+
 export class NewAccountComponent {
   @Output() accountAdded = new EventEmitter<{name: string, status: string}>();
 
-  // Injection
-  constructor(private loggingService: LoggingService) {
+  
+  constructor(private loggingService: LoggingService) { // Injecting logging service
 
   }
 
@@ -19,6 +21,6 @@ export class NewAccountComponent {
       name: accountName,
       status: accountStatus
     });
-    console.log('A server status changed, new status: ' + accountStatus);
+    this.loggingService.logStatusChange(accountStatus);
   }
 }
